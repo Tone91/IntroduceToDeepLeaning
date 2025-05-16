bb627f3cc4cd9d5a41fe57a353dd6a17e6f4d4b3
c050ffw <Tone.91>
2025-05-16 16:00:16 +0900

    add ffnnFunction.py

---
+++ ffnnFunction.py
@@ -0,0 +1,88 @@
+# %%
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+import matplotlib.pyplot as plt
+
+# %%
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(60000, 784)
+x_train = x_train/255.
+x_test = x_test.reshape(10000, 784)
+x_test = x_test/255.
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+tsb = TensorBoard(log_dir='./logs')
+
+# %%
+# モデルの構築
+input = Input(shape=(784, ))
+middle = Dense(units=64, activation='relu')(input)
+output = Dense(units=10, activation='softmax')(middle)
+model = Model(inputs=[input], outputs=[output])
+
+# %%
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# %%
+history_adam = model.fit(
+    x_train,
+    y_train,
+    batch_size=32,
+    epochs=20,
+    callbacks=[tsb],
+    validation_split=0.2
+)
+
+# %%
+acc = history_adam.history['accuracy']
+val_acc = history_adam.history['val_accuracy']
+loss = history_adam.history['loss']
+val_loss = history_adam.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+# %% 2×2のサブプロットを作成
+fig, axs = plt.subplots(2, 2, figsize=(14, 10))  # 2行2列のグラフ
+
+# Accuracy per Epoch
+axs[0, 0].plot(epochs, acc, 'bo-', label='Training Accuracy')
+axs[0, 0].set_title('Accuracy per Epoch')
+axs[0, 0].set_xlabel('Epochs')
+axs[0, 0].set_ylabel('Accuracy')
+axs[0, 0].legend()
+axs[0, 0].grid(True)
+
+# Loss per Epoch
+axs[0, 1].plot(epochs, loss, 'ro-', label='Training Loss')
+axs[0, 1].set_title('Loss per Epoch')
+axs[0, 1].set_xlabel('Epochs')
+axs[0, 1].set_ylabel('Loss')
+axs[0, 1].legend()
+axs[0, 1].grid(True)
+
+# Validation Accuracy per Epoch
+axs[1, 0].plot(epochs, val_acc, 'go-', label='Validation Accuracy')
+axs[1, 0].set_title('Validation Accuracy per Epoch')
+axs[1, 0].set_xlabel('Epochs')
+axs[1, 0].set_ylabel('Accuracy')
+axs[1, 0].legend()
+axs[1, 0].grid(True)
+
+# Validation Loss per Epoch
+axs[1, 1].plot(epochs, val_loss, 'mo-', label='Validation Loss')
+axs[1, 1].set_title('Validation Loss per Epoch')
+axs[1, 1].set_xlabel('Epochs')
+axs[1, 1].set_ylabel('Loss')
+axs[1, 1].legend()
+axs[1, 1].grid(True)
+
+# グラフのレイアウト調整
+plt.tight_layout()
+plt.savefig('C:\\Users\\user\\Documents\\PythonProjects\\tf\\ffnnFunction.png')
+plt.show()
